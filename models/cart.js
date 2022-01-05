c55db645773a77a03bfdb939492da402588f7b75
Joshua Lokken <joshualokken@pm.me>
2022-01-05 16:06:54 -0600

    Fix no product issue

--- models/cart.js
+++ models/cart.js
@@ -47,6 +47,9 @@ module.exports = class Cart {
       }
       const updatedCart = { ...JSON.parse(fileContent) };
       const product = updatedCart.products.find((product) => product.id === id);
+      if (!product) {
+        return;
+      }
       updatedCart.products = updatedCart.products.filter(
         (product) => product.id !== id
       );
