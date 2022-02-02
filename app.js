d2501a9de1459107d6c40a3fa58bae4414642eb9
Joshua Lokken <joshualokken@pm.me>
2022-02-02 13:13:15 -0600

    Add mongoose, start updating CRUD operations for mongoose

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database").mongoConnect;
 const User = require("./models/user");
 
 const app = express();
@@ -31,6 +31,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect(
+    "mongodb+srv://node-complete:node-complete@nodejs-complete.vbaer.mongodb.net/shop?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    app.listen(3000);
+    console.log("Database connected");
+  })
+  .catch((error) => console.log(error));
