e126a11474f9f8187e3f096a23eb31e1a5af9f1a
Joshua Lokken <joshualokken@pm.me>
2022-03-03 17:26:49 -0600

    Update

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,22 @@
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const User = require('../models/user');
+
+const transport = nodemailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key: process.env.SENDGRID_API_KEY,
+    },
+  })
+);
 
 exports.getSignup = (req, res, next) => {
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Sign Up',
+    errorMessage: req.flash('error'),
   });
 };
 
@@ -14,6 +25,7 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
+        req.flash('error', 'Email address is in use.');
         return res.redirect('/signup');
       }
       return bcrypt
@@ -28,9 +40,15 @@ exports.postSignup = (req, res, next) => {
         })
         .then((result) => {
           res.redirect('/login');
-        });
+          return transport.sendMail({
+            to: email,
+            from: 'joshualokken@pm.me',
+            subject: 'Signup succeeded',
+            html: '<h1>You successfully signed up at the Node Shop!</h1>',
+          });
+        })
+        .catch((error) => console.log(error));
     })
-
     .catch((error) => console.log(error));
 };
 
@@ -38,6 +56,7 @@ exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
+    errorMessage: req.flash('error'),
   });
 };
 
@@ -47,6 +66,7 @@ exports.postLogin = (req, res, next) => {
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
+        req.flash('error', 'Invalid email or password');
         return res.redirect('/login');
       }
 
@@ -61,6 +81,7 @@ exports.postLogin = (req, res, next) => {
               res.redirect('/');
             });
           }
+          req.flash('error', 'Invalid email or password');
           res.redirect('/login');
         })
         .catch((error) => {
